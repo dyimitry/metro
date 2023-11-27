2d60b1ba91206dfa525161d28f8c73ec4621b0c6
Дмитрий Шинкарев <dimonshinkariov111@yandex.ru>
2023-11-28 00:41:48 +0300

    set up alchemy postgres and alembic through docker-compose

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.8-slim
+
+RUN mkdir /src
+
+COPY ./requirements.txt /src
+
+RUN pip install -r /src/requirements.txt --no-cache-dir
+
+COPY ./.env /src
+
+COPY ./alembic.ini /src
+COPY ./alembic /src/alembic
+
+COPY ./auchan /src
+COPY ./db.py /src
+COPY ./main.py /src
+
+WORKDIR /src
---
+++ db.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+import psycopg2  # noqa
+
+from sqlalchemy import create_engine, Column, Integer, String, Float
+
+from sqlalchemy.orm import declarative_base, Session
+
+Base = declarative_base()
+load_dotenv()
+
+engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
+Base.metadata.create_all(engine)
+session = Session(engine)
+
+
+class Product(Base):
+    __tablename__ = 'Product'
+    id = Column(Integer, primary_key=True)
+    region_id = Column(Integer)
+    merchart_id = Column(Integer)
+    sub_category_code = Column(String(200))
+    product_id = Column(Integer)
+    product_name = Column(String(200))
+    product_brand = Column(String(200))
+    product_price = Column(Float)
+    product_promo_price = Column(Float)
+    product_url = Column(String(200))
+
+
